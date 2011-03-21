d32098a76abf77d666be9a00578e8d2ed25563e5
Rafaesp <sir.aradan@gmail.com>
2011-03-21 12:43:29 +0100

    UI principal y nueva clase

---
+++ Silenciador/src/com/silenciador/New.java
@@ -0,0 +1,13 @@
+package com.silenciador;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class New extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_xml);
+    }
+}
