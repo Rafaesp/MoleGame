963d6c5d7a7065d4709fa085b25705c9c754a3b0
alexmacan <xusty_alex@hotmail.com>
2011-03-21 18:49:45 +0100

    Iniciacion Topos

---
+++ Topos/src/com/androidsamples/topos.java
@@ -0,0 +1,13 @@
+package com.androidsamples;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class topos extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
