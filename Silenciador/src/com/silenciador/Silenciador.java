7a7319c0b48a7103ada6f37013650fe9f828ff7a
Rafaesp <sir.aradan@gmail.com>
2011-03-21 11:42:39 +0100

    Primer commit

---
+++ Silenciador/src/com/silenciador/Silenciador.java
@@ -0,0 +1,13 @@
+package com.silenciador;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Silenciador extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
