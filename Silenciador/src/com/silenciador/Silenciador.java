c66b3ea9acc712c4d04c007e73e517756ed41bef
alexmacan <xusty_alex@hotmail.com>
2011-03-21 18:37:25 +0100

    Quitado comentario

--- Silenciador/src/com/silenciador/Silenciador.java
+++ Silenciador/src/com/silenciador/Silenciador.java
@@ -6,7 +6,6 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 public class Silenciador extends Activity implements OnClickListener{
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
