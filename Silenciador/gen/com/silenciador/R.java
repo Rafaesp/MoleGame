82889746c290d47897cdd56e5549158d4f063c14
Rafaesp <sir.aradan@gmail.com>
2011-03-21 14:50:49 +0100

    Avance en la UI new_xml

--- Silenciador/gen/com/silenciador/R.java
+++ Silenciador/gen/com/silenciador/R.java
@@ -8,15 +8,24 @@
 package com.silenciador;
 
 public final class R {
+    public static final class array {
+        public static final int days=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050002;
-        public static final int linearLayout1=0x7f050000;
-        public static final int linearLayout2=0x7f050001;
+        public static final int button1=0x7f060002;
+        public static final int linearLayout1=0x7f060000;
+        public static final int linearLayout2=0x7f060001;
+        public static final int spinner1=0x7f060004;
+        public static final int spinner2=0x7f060007;
+        public static final int textView1=0x7f060003;
+        public static final int textView2=0x7f060006;
+        public static final int timePicker1=0x7f060005;
+        public static final int timePicker2=0x7f060008;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -25,6 +34,8 @@ public final class R {
     public static final class string {
         public static final int add=0x7f040002;
         public static final int app_name=0x7f040001;
+        public static final int endTime=0x7f040004;
         public static final int hello=0x7f040000;
+        public static final int startTime=0x7f040003;
     }
 }
--- Silenciador/src/com/silenciador/Silenciador.java
+++ Silenciador/src/com/silenciador/Silenciador.java
@@ -2,12 +2,19 @@ package com.silenciador;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class Silenciador extends Activity {
+public class Silenciador extends Activity implements OnClickListener{
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+	public void onClick(View v) {
+		
+	}
+    
 }
