fe80791db4f21c1f6fc05707d2ea50f38dd71a26
Rafaesp <sir.aradan@gmail.com>
2011-03-22 23:02:06 +0100

    nada

--- Topos/src/com/androidsamples/ToposGameView.java
+++ Topos/src/com/androidsamples/ToposGameView.java
@@ -1,10 +1,7 @@
 package com.androidsamples;
 
 import android.content.Context;
-import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.view.View;
-import android.widget.ToggleButton;
 
 public class ToposGameView extends View{
 
