84e576755a9099d794f26a849acb517c4db59b26
alexmacan <xusty_alex@hotmail.com>
2011-03-24 13:35:59 +0100

    anadido bad.png

--- Topos/src/com/androidsamples/Sprite.java
+++ Topos/src/com/androidsamples/Sprite.java
@@ -1,77 +1,109 @@
 package com.androidsamples;
 
+import java.util.Random;
+
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
- 
-
 public class Sprite {
-		private static final int BMP_COLUMNS=3;
-		private static final int BMP_ROWS=4;
-		private int x = 0; 
-		private int y = 0; 
-		private int currentFrame;
 
-       private int xSpeed = 5;
+	// direction = 0 up, 1 left, 2 down, 3 right,
 
-       private ToposGameView gameView;
+	// animation = 3 back, 1 left, 0 front, 2 right
 
-       private Bitmap bmp;
+	int[] DIRECTION_TO_ANIMATION_MAP = { 3, 1, 0, 2 };
 
-       private int width;
-       private int height;
-       
-      
+	private static final int BMP_ROWS = 4;
 
-       public Sprite(ToposGameView gameView, Bitmap bmp) {
+	private static final int BMP_COLUMNS = 3;
 
-             this.gameView=gameView;
+	private int x = 0;
 
-             this.bmp=bmp;
-             this.width=bmp.getWidth()/BMP_COLUMNS;
-             this.height=bmp.getHeight()/BMP_ROWS;
+	private int y = 0;
 
-       }
+	private int xSpeed = 5;
 
- 
+	private ToposGameView gameView;
 
-       private void update() {
+	private Bitmap bmp;
 
-             if (x > gameView.getWidth() - bmp.getWidth()/BMP_COLUMNS - xSpeed) {
+	private int currentFrame = 0;
 
-                    xSpeed = -5;
+	private int width;
 
-             }
+	private int height;
 
-             if (x + xSpeed< 0) {
+	private int ySpeed;
 
-                    xSpeed = 5;
+	public Sprite(ToposGameView gameView, Bitmap bmp) {
 
-             }
+		this.width = bmp.getWidth() / BMP_COLUMNS;
 
-             x = x + xSpeed;
-             currentFrame = ++currentFrame%BMP_COLUMNS;
+		this.height = bmp.getHeight() / BMP_ROWS;
 
-       }
+		this.gameView = gameView;
 
-      
+		this.bmp = bmp;
 
-       public void onDraw(Canvas canvas) {
+		Random rnd = new Random(System.currentTimeMillis());
 
-             update();
+		xSpeed = rnd.nextInt(50) - 5;
 
-            
-			int srcX = currentFrame * width;
+		ySpeed = rnd.nextInt(50) - 5;
 
-             int srcY = 1 * height;
+	}
 
-             Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
+	private void update() {
 
-             Rect dst = new Rect(x, y, x + width, y + height);
-             
-             canvas.drawBitmap(bmp, src , dst , null);
+		if (x >= gameView.getWidth() - width - xSpeed || x + xSpeed <= 0) {
 
-       }
+			xSpeed = -xSpeed;
 
-}   
+		}
+
+		x = x + xSpeed;
+
+		if (y >= gameView.getHeight() - height - ySpeed || y + ySpeed <= 0) {
+
+			ySpeed = -ySpeed;
+
+		}
+
+		y = y + ySpeed;
+
+		currentFrame = ++currentFrame % BMP_COLUMNS;
+
+	}
+
+	public void onDraw(Canvas canvas) {
+
+		update();
+
+		int srcX = currentFrame * width;
+
+		int srcY = getAnimationRow() * height;
+
+		Rect src = new Rect(srcX, srcY, srcX + width, srcY + height);
+
+		Rect dst = new Rect(x, y, x + width, y + height);
+
+		canvas.drawBitmap(bmp, src, dst, null);
+
+	}
+
+	// direction = 0 up, 1 left, 2 down, 3 right,
+
+	// animation = 3 back, 1 left, 0 front, 2 right
+
+	private int getAnimationRow() {
+
+		double dirDouble = (Math.atan2(xSpeed, ySpeed) / (Math.PI / 2) + 2);
+
+		int direction = (int) Math.round(dirDouble) % BMP_ROWS;
+
+		return DIRECTION_TO_ANIMATION_MAP[direction];
+
+	}
+
+}
