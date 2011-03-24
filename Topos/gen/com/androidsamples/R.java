5eda0264e2b5b46c402d1d6b1c013a26e1323736
alexmacan <xusty_alex@hotmail.com>
2011-03-24 13:02:46 +0100

    anadido Sprites

--- Topos/gen/com/androidsamples/R.java
+++ Topos/gen/com/androidsamples/R.java
@@ -23,9 +23,8 @@ public final class R {
         public static final int textView1=0x7f050001;
     }
     public static final class layout {
-        public static final int gameview=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int settings=0x7f030002;
+        public static final int main=0x7f030000;
+        public static final int settings=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
