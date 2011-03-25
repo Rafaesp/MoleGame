6c935b5e3d6dd9f251c74394364fc409950bb37b
alexmacan <xusty_alex@hotmail.com>
2011-03-25 10:49:29 +0100

    Probando OnClick en GameToposView

--- Topos/src/com/androidsamples/ToposGameView.java
+++ Topos/src/com/androidsamples/ToposGameView.java
@@ -30,46 +30,47 @@ public class ToposGameView extends SurfaceView implements OnClickListener {
 	private Bitmap bmp;
 	private SurfaceHolder holder;
 	private GameLoopThread gameLoopThread;;
+
 	private Sprite mole1;
-	private Sprite mole2;
-	private Sprite mole3;
-	private Sprite mole4;
-	private Sprite mole5;
-	private Sprite mole6;
-	private Sprite mole7;
-	private Sprite mole8;
-	private Sprite mole9;
-	private Sprite mole10;
-	private Sprite mole11;
-	private Sprite mole12;
 
 
 	private List<Sprite> moles;
-
+	//NO BORRAR TODAVIA COMENTARIOS
+	//NO BORRAR TODAVIA COMENTARIOS
+	//NO BORRAR TODAVIA COMENTARIOS
 
 	public ToposGameView(Context context) {
 		super(context);
 
 		moles = new ArrayList<Sprite>();
+		
 
 		gameLoopThread = new GameLoopThread(this);
 		bmp = BitmapFactory.decodeResource(getResources(), R.drawable.bad1);
 
 		setOnClickListener(this);// TODO
-
-		for(int i = 0; i<4; i++){
-			moles.add(new Sprite(this, this.bmp, WIDTH/4, (2*HEIGHT)/8));
-			moles.add(new Sprite(this, this.bmp, (2*WIDTH)/4, (2*HEIGHT)/8));
-			moles.add(new Sprite(this, this.bmp, (3*WIDTH)/4, (2*HEIGHT)/8));
-			
-			moles.add(new Sprite(this, this.bmp, WIDTH/4, (3*HEIGHT)/8));
-			moles.add(new Sprite(this, this.bmp, (2*WIDTH)/4, (3*HEIGHT)/8));
-			moles.add(new Sprite(this, this.bmp, (3*WIDTH)/4, (3*HEIGHT)/8));
-			
-			moles.add(new Sprite(this, this.bmp, WIDTH/4, (4*HEIGHT)/8));
-			moles.add(new Sprite(this, this.bmp, (2*WIDTH)/4, (4*HEIGHT)/8));
-			moles.add(new Sprite(this, this.bmp, (3*WIDTH)/4, (4*HEIGHT)/8));
-		}
+		
+		Log.i("Elemento clickeado en onClick","1");
+		mole1 = (Sprite) findViewById(R.drawable.mole1);
+		Log.i("Elemento clickeado en onClick","2");
+		mole1.setOnClickListener(this);
+		Log.i("Elemento clickeado en onClick","3");//no llega
+		mole1=new Sprite(this, this.bmp, WIDTH/4, (2*HEIGHT)/8);
+		Log.i("Elemento clickeado en onClick","4");
+		
+//		for(int i = 0; i<4; i++){
+//			moles.add(new Sprite(this, this.bmp, WIDTH/4, (2*HEIGHT)/8));
+//			moles.add(new Sprite(this, this.bmp, (2*WIDTH)/4, (2*HEIGHT)/8));
+//			moles.add(new Sprite(this, this.bmp, (3*WIDTH)/4, (2*HEIGHT)/8));
+//			
+//			moles.add(new Sprite(this, this.bmp, WIDTH/4, (3*HEIGHT)/8));
+//			moles.add(new Sprite(this, this.bmp, (2*WIDTH)/4, (3*HEIGHT)/8));
+//			moles.add(new Sprite(this, this.bmp, (3*WIDTH)/4, (3*HEIGHT)/8));
+//			
+//			moles.add(new Sprite(this, this.bmp, WIDTH/4, (4*HEIGHT)/8));
+//			moles.add(new Sprite(this, this.bmp, (2*WIDTH)/4, (4*HEIGHT)/8));
+//			moles.add(new Sprite(this, this.bmp, (3*WIDTH)/4, (4*HEIGHT)/8));
+//		}
 
 		Log.i("Ancho", ""+WIDTH);//TODO
 		Log.i("Alto", ""+HEIGHT);
@@ -127,23 +128,30 @@ public class ToposGameView extends SurfaceView implements OnClickListener {
 	@Override
 	public void onClick(View v) { //TODO I don't like. Must be another way.
 		
-
-			//TODO No he podido hacer mas pruebas, esto se deberia hacer con un swich case, pero lo que pasa 
-			// es que al no estar nombrados en algun XML los sprites, no tienen un id en R, entonces el problema que esto causa es
-			// que no sabemos con que atributo compararlo es decir con if(v.getId()==clicked.getId()) todos entran en el if
-			// y haciendo un casting como aparece ahora no entra ninguna vez, no se como identificar la View en la que se ha hecho click
-			for(Sprite clicked : moles){
-				if(v.equals((View) clicked) ){
-					
-					Log.i("onClick","Debe aparecer una vez");
-					clicked.setBeating(true); //TODO
-											  //ya se que este metodo para hacer click en un solo elemento es muy "guarro", pero no debe estar muy
-											  // lejos de lo real, que seguro que es un onDraw(en Sprite) con distintos casos, parecido a cuando trabajamos
-											  // con botones.
-					clicked.setBeaten();
-					break;
-				}
-			}
+if(v.getId()==mole1.getId()){
+	mole1.setBeating(true);
+	mole1.setBeaten();
+	Log.i("Elemento clickeado en onClick",mole1.toString());
+}
+		
+		
+		
+//			//TODO No he podido hacer mas pruebas, esto se deberia hacer con un swich case, pero lo que pasa 
+//			// es que al no estar nombrados en algun XML los sprites, no tienen un id en R, entonces el problema que esto causa es
+//			// que no sabemos con que atributo compararlo es decir con if(v.getId()==clicked.getId()) todos entran en el if
+//			// y haciendo un casting como aparece ahora no entra ninguna vez, no se como identificar la View en la que se ha hecho click
+//			for(Sprite clicked : moles){
+//				//if(v.equals((View) clicked) ){
+//					
+//					Log.i("Elemento clickeado en onClick",clicked.toString());
+//					clicked.setBeating(true); //TODO
+//											  //ya se que este metodo para hacer click en un solo elemento es muy "guarro", pero no debe estar muy
+//											  // lejos de lo real, que seguro que es un onDraw(en Sprite) con distintos casos, parecido a cuando trabajamos
+//											  // con botones.
+//					clicked.setBeaten();
+//					break;
+//				//}
+//			}
 			
 	
 		
